857a04641861b1151827645cec3b6d0c756d8624
serendipity <fe91990@gmail.com>
2019-09-01 18:54:31 +0900

    reflected feedback
    1. intend
    2. let replaced var

--- Back_end_assignment/4/calculateByFile.js
+++ Back_end_assignment/4/calculateByFile.js
@@ -9,35 +9,37 @@ const divide = calc.divide;
 
 const inspection = new Promise(resolve=>
     fs.readFile('input.txt',(err,data)=>{
-    if(err) { throw err;}
-    
-    resolve(data.toString());
-    
-    
-}));
+        if(err) { throw err;}
+        resolve(data.toString());
+        }
+    )
+);
 
 
 inspection.then(result=>
-{
-var calclist = result.split(',');
-console.log(calclist)
-if(calclist[1]==='+'){
-    return add(calclist[0],calclist[2]);
-}
-else if(calclist[1]==='-'){
-    return subtract(calclist[0],calclist[2]);
-}
-else if(calclist[1]==='*'){
-    return multiply(calclist[0],calclist[2]);
-}
-else if(calclist[1]==="/"){
-    return divide(calclist[0],calclist[2]);
-}
-else{
-    console.log("error")
-}
-}
-).then(result=>fs.writeFile('output.txt',result,err=>{if (err){throw err;} console.log("완료") }))
+    {
+        let calclist = result.split(',');
+        console.log(calclist)
+        if(calclist[1]==='+'){
+            return add(calclist[0],calclist[2]);
+        }
+        else if(calclist[1]==='-'){
+            return subtract(calclist[0],calclist[2]);
+        }
+        else if(calclist[1]==='*'){
+            return multiply(calclist[0],calclist[2]);
+        }
+        else if(calclist[1]==="/"){
+            return divide(calclist[0],calclist[2]);
+        }
+        else{
+            console.log("error")
+        }
+    }
+).then(result=>fs.writeFile('output.txt',result,err=>{
+    if (err){throw err;} 
+    console.log("완료") 
+}))
 
 
 
